7af98381700b2458a9bf5ec792c15904f0eb4031
Thomas Fossøy Lyseggen <thomas@lyseggen.xyz>
2020-04-02 20:27:30 +0200

    Add procfile and update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject groove "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :url "http://rutta.heroku.com"
+  :min-lein-version "2.0.0"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
@@ -39,5 +40,5 @@
                            :plugins [[lein-auto "0.1.3"]]
                            }
              :project/test {}
-             :uberjar {:aot :all :jvm-opts ["-Dclojure.tools.logging.factory=clojure.tools.logging.impl/slf4j-factory"]}}
+             :uberjar {:main groove.core :aot :all :jvm-opts ["-Dclojure.tools.logging.factory=clojure.tools.logging.impl/slf4j-factory"]}}
   )
